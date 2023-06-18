34a2886b8df9b98966ca1e802ffe2278a4949404
Andin <72485469+shadinn@users.noreply.github.com>
2023-06-18 10:06:00 +0700

    Update PadiPedia.py

--- PadiPedia.py
+++ PadiPedia.py
@@ -502,7 +502,6 @@ if menu == 'Clustering':
 
         # --- Combining Both Data Frame ---
         df_profile = df_cluster_summary.join(df_profile_overall).reset_index()
-        df_profile.style.background_gradient(cmap='YlOrBr').hide_index()
         st.write(df_profile)
 
         # Hasil Cluster
